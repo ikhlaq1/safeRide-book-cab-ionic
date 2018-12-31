45ccc0ef37a5520bdf2c132c0219693b7465e6e2
Ikhlaq Firdous <ikhlaq.firdous@blackstraw.ai>
2018-12-31 20:36:37 +0530

    register modification

--- src/pages/register-passenger/register-passenger.ts
+++ src/pages/register-passenger/register-passenger.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
-/**
- * Generated class for the RegisterPassengerPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-register-passenger',
